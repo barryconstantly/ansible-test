fe35eb66918f5ce03be03c369f1261a84520ba00
barryconstantly <82120508+barryconstantly@users.noreply.github.com>
2021-05-03 21:09:27 +0000

    Add files via upload

--- config_strip.py
+++ config_strip.py
@@ -1,9 +1,9 @@
-config_file = open('/tmp/config.txt')
+config_file = open('config.txt')
 config_list = config_file.readlines()
 config_file.close
 
 # Now open again and overwrite
-config_file = open('/tmp/config.txt', 'w')
+config_file = open('config.txt', 'w')
 for i in range(8,len(config_list)-3):
 	config_line = config_list[i]
 	if 'no aaa new-model' in config_line or 'login' in config_line:

b11137bbf64a729df0846e11bc9003b43dc1d461
barryconstantly <82120508+barryconstantly@users.noreply.github.com>
2021-05-23 19:56:25 -0400

    Update config_strip.py

--- config_strip.py
+++ config_strip.py
@@ -4,7 +4,7 @@ config_file.close
 
 # Now open again and overwrite
 config_file = open('config_scrubbed.txt', 'w')
-for i in range(3,len(config_list)):
+for i in range(4,len(config_list)):
 	config_line = config_list[i].rstrip()
 	config_line = config_line.replace('\r','')
 	if 'no aaa new-model' in config_line or 'login' in config_line:

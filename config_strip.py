d28e02f50af2edff2e3e572cb7e00a2ef0ca6de4
barryconstantly <82120508+barryconstantly@users.noreply.github.com>
2021-05-23 17:55:05 -0400

    Update config_strip.py

--- config_strip.py
+++ config_strip.py
@@ -3,7 +3,7 @@ config_list = config_file.readlines()
 config_file.close
 
 # Now open again and overwrite
-config_file = open('config.txt', 'w')
+config_file = open('config_scrubbed.txt', 'w')
 for i in range(3,len(config_list)):
 	config_line = config_list[i].rstrip()
 	config_line = config_line.replace('\r','')

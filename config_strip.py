18addfdfca437355425ad9edbbfbba57a767db42
barryconstantly <82120508+barryconstantly@users.noreply.github.com>
2021-05-24 15:59:44 +0000

    Add files via upload

---
+++ config_strip.py
@@ -0,0 +1,18 @@
+config_file = open('config.txt')
+config_list = config_file.readlines()
+config_file.close
+
+# Now open again and overwrite
+config_file = open('config_scrubbed.txt', 'w')
+for i in range(4,len(config_list)-2):
+#	config_line = config_list[i]
+	config_line = config_list[i].rstrip()
+	config_line = config_line.replace('\r','')
+	if 'no aaa new-model' in config_line or 'login' in config_line:
+		pass
+	else:		
+#		config_file.write(config_line)
+		config_file.write(config_line+'\n')
+config_file.write('\n')
+config_file.close()
+
